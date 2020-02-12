1ad7dad18e51c0423885c5d4dca490c1318c963c
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-11 20:49:41 -0500

    adding indents

--- module2-oop-code-style-and-reviews/Making a class.py
+++ module2-oop-code-style-and-reviews/Making a class.py
@@ -3,29 +3,43 @@
 Define a Pet class with their attributes
 """
 
-""" Setting up the class attribute, same for all instances"""
+    """ 
+    Setting up the class attribute, same for all instances
+    """
 class Pet(object):
     eats = 'food'
 
-"""Define the init method"""
     def __init__(self, name):
+    """
+    Define the init method
+    """
         self.name = name
 
-""" Instance Attribute, different for each instance"""
+    """ 
+    Instance Attribute, different for each instance
+    """
     def speaks(self):
         print(self.name + ' speaks')
 
-"""Define a method for speaking behavior"""
+    """
+    Define a method for speaking behavior
+    """
     def eat(self, meal):
-"""Define a method for eating behavior"""
+    """
+    Define a method for eating behavior
+    """
         if meal == self.eats:
             print('Bark!')
         else:
             print('GRR!')
 
-"""Create an instance of Class"""
+    """
+    Create an instance of Class
+    """
 my_pet = Pet('Dog')
-"""Calling the method on the new instance"""
+    """
+    Calling the method on the new instance
+    """
 my_pet.speak()
 my_pet.eat('food')
    
